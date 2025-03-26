0a92111d921daeaa292d88112fe9c7fae643dac3
Jonathan G. Hecl <gshaxor@gmail.com>
2025-03-26 01:06:41 -0300

    first release

--- README.md
+++ README.md
@@ -3,11 +3,28 @@ Efficient Selection, Perfect Fit
 
 This application efficiently selects folders from a given list that best fit within a user-defined maximum storage capacity. It calculates the size of each source folder and optimizes the selection to maximize the utilization of the available space, minimizing any unused space. This utility is ideal for users who need to identify the optimal set of folders to fit within a specific storage limit.
 
-## How to Use
+## 📚 How to Use
 
 ```sh
-> folderfit <sources> <totalsize>
+> folderfit <sources> -size=<totalsize> [-verbose]
 ```
 
 - `<sources>`: A list of folders to be selected from.
 - `<totalsize>`: The total storage capacity in bytes.
+- `-verbose`: Optional flag to enable verbose output.
+
+## Example
+
+```sh
+> folderfit file50kb.dummy file100kb.dummy -size=150000
+```
+
+## 🤝 Contributing
+
+Contributions are welcome! Feel free to submit issues and pull requests.
+
+## 🔗 Links
+
+- [GitHub Repository](https://github.com/jonathanhecl/folderfit)
+- [Report Issues](https://github.com/jonathanhecl/folderfit/issues)
+- [Releases](https://github.com/jonathanhecl/folderfit/releases)
--- main.go
+++ main.go
@@ -1,12 +1,162 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
 
 var version = "0.0.1"
+
+func printUsage() {
+	fmt.Println("\nUsage: folderfit <sources> -size=<totalsize> [-verbose]")
+}
 
 func main() {
 	fmt.Println("FolderFit v", version)
 
-	// How to use:
-	// folderfit <sources> <totalsize>
+	if len(os.Args) < 3 {
+		printUsage()
+		return
+	}
+
+	var totalSize int = 0
+	var sources []string
+	var verbose bool
+
+	for _, arg := range os.Args[1:] {
+		if strings.HasPrefix(arg, "-size=") {
+			sizeStr := strings.TrimPrefix(arg, "-size=")
+			var err error
+			totalSize, err = strconv.Atoi(sizeStr)
+			if err != nil {
+				fmt.Println("Invalid size argument")
+				return
+			}
+		} else if arg == "-verbose" {
+			verbose = true
+		} else {
+			sources = append(sources, arg)
+		}
+	}
+
+	if totalSize == 0 {
+		fmt.Println("Invalid size argument")
+		printUsage()
+		return
+	}
+
+	if verbose {
+		fmt.Println()
+		fmt.Println("Analyzing...")
+	}
+	folderSizes := make(map[string]int)
+	for _, source := range sources {
+		folderSizes[source] = calculateSize(source)
+	}
+
+	if verbose {
+		for name, size := range folderSizes {
+			fmt.Printf("%s - %s\n", name, formatSize(size))
+		}
+	}
+
+	if verbose {
+		fmt.Printf("\nTotal target size: %s\n\n", formatSize(totalSize))
+	}
+
+	selected := selectBestFolders(folderSizes, totalSize)
+
+	if len(selected) == 0 {
+		fmt.Println("No selection possible")
+		return
+	}
+
+	if verbose {
+		fmt.Println("Selected:")
+	}
+	for name, size := range selected {
+		fmt.Printf("%s - %s\n", name, formatSize(size))
+	}
+	fmt.Printf("\nSelection size: %s / %s\n", formatSize(calculateTotalSize(selected)), formatSize(totalSize))
+	fmt.Printf("Free space: %s\n", formatSize(totalSize-calculateTotalSize(selected)))
+}
+
+func calculateSize(source string) int {
+	var totalSize int
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return 0
+	}
+
+	if info.IsDir() {
+		files, err := os.ReadDir(source)
+		if err != nil {
+			return 0
+		}
+		for _, file := range files {
+			totalSize += calculateSize(filepath.Join(source, file.Name()))
+		}
+	} else {
+		totalSize = int(info.Size())
+	}
+
+	return totalSize
+}
+
+func selectBestFolders(folderSizes map[string]int, totalSize int) map[string]int {
+	names := make([]string, 0, len(folderSizes))
+	sizes := make([]int, 0, len(folderSizes))
+	for name, size := range folderSizes {
+		names = append(names, name)
+		sizes = append(sizes, size)
+	}
+
+	n := len(names)
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, totalSize+1)
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= totalSize; j++ {
+			if sizes[i-1] <= j {
+				dp[i][j] = max(dp[i-1][j], dp[i-1][j-sizes[i-1]]+sizes[i-1])
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	selected := make(map[string]int)
+	j := totalSize
+	for i := n; i > 0 && dp[i][j] != 0; i-- {
+		if dp[i][j] != dp[i-1][j] {
+			selected[names[i-1]] = sizes[i-1]
+			j -= sizes[i-1]
+		}
+	}
+
+	return selected
+}
+
+func calculateTotalSize(folderSizes map[string]int) int {
+	total := 0
+	for _, size := range folderSizes {
+		total += size
+	}
+	return total
+}
+
+func formatSize(size int) string {
+	if size < 1024 {
+		return fmt.Sprintf("%d bytes", size)
+	} else if size >= 1024 && size < 1024*1024 {
+		return fmt.Sprintf("%.2f KB", float64(size)/1024)
+	} else {
+		return fmt.Sprintf("%.2f MB", float64(size)/(1024*1024))
+	}
 }
